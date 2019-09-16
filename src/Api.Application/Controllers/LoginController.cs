fa593994d6f030948c4516510609dd8247db684e
Lucas Patriarca <lucas.patriarca@hbsis.com.br>
2019-09-16 17:52:09 -0300

    adicionando serviço de login

--- src/Api.Application/Controllers/LoginController.cs
+++ src/Api.Application/Controllers/LoginController.cs
@@ -1,7 +1,40 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Api.Domain.Entities;
+using Api.Domain.Interfaces.Services.Users;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Api.Application.Controllers
 {
-    public class LoginController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
     {
-        
+        [HttpPost]
+        public async Task<object> Login([FromBody] UserEntity userEntity, [FromServices] ILoginService service)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (userEntity == null)
+                return BadRequest();
+
+            try
+            {
+                var result = await service.FindByLogin(userEntity);
+                
+                if (result == null)
+                    return NotFound();
+                
+                return Ok(result);
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+
+
+        }
     }
 }
